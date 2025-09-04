5bd70a1ccf0ecc2a2b51811232d7f3383011979e
Pierre Paulo <pierresantista@hotmail.com>
2025-09-04 12:26:13 -0300

    feat(products): adicionar tabs com skeleton e estrutura inicial

---
+++ src/components/products/tab.tsx
@@ -0,0 +1,14 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "../ui/tabs";
+
+export const ProductsTab = () => {
+  return (
+    <Tabs defaultValue="tab1">
+      <TabsList className="grid w-full grid-cols-2">
+        <TabsTrigger value="tab1">Tab1</TabsTrigger>
+        <TabsTrigger value="tab2">Tab2</TabsTrigger>
+      </TabsList>
+      <TabsContent value="tab1">Conteudo da TAB 1</TabsContent>
+      <TabsContent value="tab2">Conteudo da TAB 2</TabsContent>
+    </Tabs>
+  );
+};
