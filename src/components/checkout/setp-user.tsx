f9aa9f2ca35b78e697fc95f2bee99fd2dd4c8ad5
Pierre Paulo <pierresantista@hotmail.com>
2025-09-05 14:23:21 -0300

    feat(checkout): finalizar formulário de endereço no step-address

--- src/components/checkout/setp-user.tsx
+++ src/components/checkout/setp-user.tsx
@@ -1,9 +1,65 @@
+"use client";
+
 import { CheckoutSteps } from "@/types/chekout-steps";
 import { Dispatch, SetStateAction } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useCheckoutstore } from "@/stores/checkout-store";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+
+const formSchema = z.object({
+  name: z.string().min(2, "Preencha seu nome"),
+});
 
 type Props = {
   setStep: Dispatch<SetStateAction<CheckoutSteps>>;
 };
 export const StepUser = ({ setStep }: Props) => {
-  return <div></div>;
+  const { name, setName } = useCheckoutstore((state) => state);
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: { name },
+  });
+
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    setName(values.name);
+    setStep("address");
+  };
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="flex flex-col gap-4"
+      >
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Seu nome</FormLabel>
+              <FormControl>
+                <Input autoFocus placeholder="Qual seu nome?" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit" variant="outline">
+          Proximo
+        </Button>
+      </form>
+    </Form>
+  );
 };
--- src/components/checkout/step-address.tsx
+++ src/components/checkout/step-address.tsx
@@ -1,10 +1,155 @@
 import { CheckoutSteps } from "@/types/chekout-steps";
 import { Dispatch, SetStateAction } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useCheckoutstore } from "@/stores/checkout-store";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+const formSchema = z.object({
+  street: z.string().min(2, "Preencha o endreço"),
+  number: z.string().min(2, "Preencha o numero"),
+  complement: z.string().optional(),
+  district: z.string().min(2, "Preencha o bairro"),
+  city: z.string().min(2, "Preencha a cidade"),
+  state: z.string().min(2, "Preencha o estado"),
+});
 
 type Props = {
   setStep: Dispatch<SetStateAction<CheckoutSteps>>;
 };
-
 export const StepAddress = ({ setStep }: Props) => {
-  return <div></div>;
+  const { address, setAddress } = useCheckoutstore((state) => state);
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: { ...address },
+  });
+
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    setAddress(values);
+    setStep("finish");
+  };
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)}>
+        {" "}
+        <div className="grid grid-cols-2 gap-4">
+          <FormField
+            control={form.control}
+            name="street"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Rua</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="number"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Numero</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="complement"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Complemnto</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="district"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Bairro</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="city"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Cidade</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="state"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Estado</FormLabel>
+                <FormControl>
+                  <Select
+                    defaultValue={field.value}
+                    onValueChange={field.onChange}
+                  >
+                    <SelectTrigger>
+                      <SelectValue placeholder="estado"></SelectValue>
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectItem value="sp">São Paulo</SelectItem>
+                      <SelectItem value="rj">Rio de Janeiro</SelectItem>
+                      <SelectItem value="mg">Minas Gerais</SelectItem>
+                    </SelectContent>
+                  </Select>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+        <div className="flex justify-between mt-4">
+          <Button variant="link" onClick={() => setStep("user")}>
+            Voltar
+          </Button>
+          <Button type="submit">Concluir</Button>
+        </div>
+      </form>
+    </Form>
+  );
 };
