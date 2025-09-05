05e9d9505b4637ffd082c324e5aa12afb32b8f45
Pierre Paulo <pierresantista@hotmail.com>
2025-09-05 14:59:41 -0300

    feat(checkout): adicionar link para envio no WhatsApp ao concluir formulário de endereço

--- src/components/checkout/setp-finish.tsx
+++ src/components/checkout/setp-finish.tsx
@@ -1,3 +1,25 @@
+import { useCheckoutstore } from "@/stores/checkout-store";
+import { Button } from "../ui/button";
+import Link from "next/link";
+import { generateMessage } from "@/lib/generate-message";
+
 export const StepFinish = () => {
-  return <div></div>;
+  const { name } = useCheckoutstore((state) => state);
+  const message = generateMessage();
+  const linkZap = `https://wa.me//${
+    process.env.NEXT_PUBLIC_ZAP
+  }?text=${encodeURI(message)}`;
+  return (
+    <div className="text-center flex flex-col gap-5">
+      <p>
+        Perfeito <strong>{name}</strong>!
+      </p>
+      <p>Agora envise seu pedido ao nosso WhatsApp para concluir.</p>
+      <Button>
+        <Link target="_blank" href={linkZap}>
+          Enviar para WhatsApp
+        </Link>
+      </Button>
+    </div>
+  );
 };
---
+++ src/lib/generate-message.ts
@@ -0,0 +1,20 @@
+import { useCartStore } from "@/stores/cart-store";
+import { useCheckoutstore } from "@/stores/checkout-store";
+
+export const generateMessage = () => {
+  const { name, address } = useCheckoutstore((state) => state);
+  const { cart } = useCartStore((state) => state);
+
+  let orderProducts = [];
+  for (let item of cart) {
+    orderProducts.push(`${item.quantity}x ${item.product.name}`);
+  }
+
+  return `*Dados do cliente:*
+Nome: ${name}
+Endreço: ${address.street}, ${address.number} (${address.complement})
+${address.district}, ${address.city}/${address.state}
+--------
+*Pedido:*
+${orderProducts.join("\n")}`;
+};
