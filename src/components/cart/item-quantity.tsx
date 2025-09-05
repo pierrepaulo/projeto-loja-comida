811d786653e9225b4566fd9a3f8db3bdb7c9a0f3
Pierre Paulo <pierresantista@hotmail.com>
2025-09-05 12:51:56 -0300

    feat(cart): implementar funcionalidade completa de carrinho

---
+++ src/components/cart/item-quantity.tsx
@@ -0,0 +1,41 @@
+import { useCartStore } from "@/stores/cart-store";
+import { Cart } from "@/types/cart";
+import { Button } from "../ui/button";
+import { MinusIcon, PlusIcon } from "lucide-react";
+
+type Props = {
+  cartItem: Cart;
+};
+
+export const CartItemQuantity = ({ cartItem }: Props) => {
+  const handlePlusButton = () => {
+    upsertCartItem(cartItem.product, 1);
+  };
+
+  const handleMinusButton = () => {
+    upsertCartItem(cartItem.product, -1);
+  };
+
+  const { upsertCartItem } = useCartStore((state) => state);
+  return (
+    <div className="flex items-center gap-2">
+      <Button
+        onClick={handlePlusButton}
+        variant="outline"
+        size="icon"
+        className="size-5"
+      >
+        <PlusIcon className="size-3" />
+      </Button>
+      <div className="text-xs">{cartItem.quantity} </div>
+      <Button
+        onClick={handleMinusButton}
+        variant="outline"
+        size="icon"
+        className="size-5"
+      >
+        <MinusIcon className="size-3" />
+      </Button>
+    </div>
+  );
+};
--- src/components/cart/sidebar.tsx
+++ src/components/cart/sidebar.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { RocketIcon } from "lucide-react";
 import { Button } from "../ui/button";
 import {
@@ -8,29 +10,45 @@ import {
   SheetTrigger,
 } from "../ui/sheet";
 import { Separator } from "../ui/separator";
+import { useCartStore } from "@/stores/cart-store";
+import { CartItem } from "./item";
 
 export const CartSidebar = () => {
+  const { cart } = useCartStore((state) => state);
+
+  let subtotal = 0;
+  for (let item of cart) {
+    subtotal += item.quantity * item.product.price;
+  }
+
   return (
     <Sheet>
       <SheetTrigger asChild>
-        <Button>
+        <Button className="relative">
           <RocketIcon className="mr-2" />
           <p>Carrinho</p>
+          {cart.length > 0 && (
+            <div className="absolute size-3 bg-red-600 rounded-full -right-1 -top-1 "></div>
+          )}
         </Button>
       </SheetTrigger>
       <SheetContent>
         <SheetHeader>
           <SheetTitle>Carrinho</SheetTitle>
         </SheetHeader>
-        <div className="flex flex-col gap-5 my-3 px-5">...</div>
+        <div className="flex flex-col gap-5 my-3 px-5">
+          {cart.map((item) => (
+            <CartItem key={item.product.id} item={item} />
+          ))}
+        </div>
         <Separator className="my-4 " />
         <div className="flex justify-between items-center text-xs px-5">
           <div>Subtotal</div>
-          <div>...</div>
+          <div>R$ {subtotal.toFixed(2)} </div>
         </div>
         <Separator className="my-4" />
         <div className="text-center">
-          <Button>Finalizar Compra</Button>
+          <Button disabled={cart.length === 0}>Finalizar Compra</Button>
         </div>
       </SheetContent>
     </Sheet>
